1d7e803d2ccd134f0e83065ac0c7e8636dea360d
Danilo Soares Aliberti <dsaliberti@gmail.com>
2024-08-02 09:19:47 +0100

    Current Pace: draft implementation of time/distance also some formatting

--- MyWorkouts WatchKit Extension/Views/ElapsedTimeView.swift
+++ MyWorkouts WatchKit Extension/Views/ElapsedTimeView.swift
@@ -1,56 +1,71 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-The elapsed time.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ The elapsed time.
+ */
 
 import SwiftUI
 
 struct ElapsedTimeView: View {
-    var elapsedTime: TimeInterval = 0
-    var showSubseconds: Bool = true
-    @State private var timeFormatter = ElapsedTimeFormatter()
-
-    var body: some View {
-        Text(NSNumber(value: elapsedTime), formatter: timeFormatter)
-            .fontWeight(.semibold)
-            .onChange(of: showSubseconds) {
-                timeFormatter.showSubseconds = $0
-            }
-    }
+  var elapsedTime: TimeInterval = 0
+  var showSubseconds: Bool = true
+  @State private var timeFormatter = ElapsedTimeFormatter()
+  
+  var body: some View {
+    Text(NSNumber(value: elapsedTime), formatter: timeFormatter)
+      .fontWeight(.semibold)
+      .onChange(of: showSubseconds) {
+        timeFormatter.showSubseconds = $0
+      }
+  }
 }
 
 class ElapsedTimeFormatter: Formatter {
-    let componentsFormatter: DateComponentsFormatter = {
-        let formatter = DateComponentsFormatter()
-        formatter.allowedUnits = [.minute, .second]
-        formatter.zeroFormattingBehavior = .pad
-        return formatter
-    }()
-    var showSubseconds = true
-
-    override func string(for value: Any?) -> String? {
-        guard let time = value as? TimeInterval else {
-            return nil
-        }
-
-        guard let formattedString = componentsFormatter.string(from: time) else {
-            return nil
-        }
-
-        if showSubseconds {
-            let hundredths = Int((time.truncatingRemainder(dividingBy: 1)) * 100)
-            let decimalSeparator = Locale.current.decimalSeparator ?? "."
-            return String(format: "%@%@%0.2d", formattedString, decimalSeparator, hundredths)
-        }
-
-        return formattedString
+  
+  var showSubseconds = true
+  var suffix: String
+  init(
+    showSubseconds: Bool = true,
+    suffix: String = ""
+  ) {
+    self.showSubseconds = showSubseconds
+    self.suffix = suffix
+    super.init()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  let componentsFormatter: DateComponentsFormatter = {
+    let formatter = DateComponentsFormatter()
+    formatter.allowedUnits = [.minute, .second]
+    formatter.zeroFormattingBehavior = .pad
+    return formatter
+  }()
+  
+  override func string(for value: Any?) -> String? {
+    guard let time = value as? TimeInterval else {
+      return nil
     }
+    
+    guard let formattedString = componentsFormatter.string(from: time) else {
+      return nil
+    }
+    
+    if showSubseconds {
+      let hundredths = Int((time.truncatingRemainder(dividingBy: 1)) * 100)
+      let decimalSeparator = Locale.current.decimalSeparator ?? "."
+      return String(format: "%@%@%0.2d%@", formattedString, decimalSeparator, hundredths, suffix)
+    }
+    
+    return formattedString.appending(suffix)
+  }
 }
 
 struct ElapsedTime_Previews: PreviewProvider {
-    static var previews: some View {
-        ElapsedTimeView()
-    }
+  static var previews: some View {
+    ElapsedTimeView()
+  }
 }
--- MyWorkouts WatchKit Extension/Views/MetricsView.swift
+++ MyWorkouts WatchKit Extension/Views/MetricsView.swift
@@ -1,58 +1,125 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-The workout metrics view.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ The workout metrics view.
+ */
 
 import SwiftUI
 import HealthKit
 
 struct MetricsView: View {
-    @EnvironmentObject var workoutManager: WorkoutManager
-    
-    var body: some View {
-        TimelineView(MetricsTimelineSchedule(from: workoutManager.builder?.startDate ?? Date(),
-                                             isPaused: workoutManager.session?.state == .paused)) { context in
-            VStack(alignment: .leading) {
-                ElapsedTimeView(elapsedTime: workoutManager.builder?.elapsedTime(at: context.date) ?? 0, showSubseconds: context.cadence == .live)
-                    .foregroundStyle(.yellow)
-                Text(Measurement(value: workoutManager.activeEnergy, unit: UnitEnergy.kilocalories)
-                        .formatted(.measurement(width: .abbreviated, usage: .workout, numberFormatStyle: .number.precision(.fractionLength(0)))))
-                Text(workoutManager.heartRate.formatted(.number.precision(.fractionLength(0))) + " bpm")
-                Text(Measurement(value: workoutManager.distance, unit: UnitLength.meters).formatted(.measurement(width: .abbreviated, usage: .road)))
-            }
-            .font(.system(.title, design: .rounded).monospacedDigit().lowercaseSmallCaps())
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .ignoresSafeArea(edges: .bottom)
-            .scenePadding()
+  @EnvironmentObject var workoutManager: WorkoutManager
+  
+  var pace: Double {
+    workoutManager.distance > 0
+    ? Double(workoutManager.builder?.elapsedTime ?? 0.0) /  workoutManager.distance
+    : Double(0)
+  }
+  
+  @State private var timeFormatter = ElapsedTimeFormatter(showSubseconds: true, suffix: "/km")
+  
+  var body: some View {
+    TimelineView(
+      MetricsTimelineSchedule(
+        from: workoutManager.builder?.startDate ?? Date(),
+        isPaused: workoutManager.session?.state == .paused
+      )
+    ) { context in
+      
+      VStack(alignment: .leading, spacing: .zero) {
+        ElapsedTimeView(
+          elapsedTime: workoutManager.builder?.elapsedTime(at: context.date) ?? 0, 
+          showSubseconds: context.cadence == .live
+        )
+        .foregroundStyle(.yellow)
+        .padding([.top, .leading], 16)
+        
+        ScrollView {
+          VStack(alignment: .leading, spacing: .zero) {
+            Text(
+              Measurement(
+                value: workoutManager.activeEnergy, unit: UnitEnergy.kilocalories
+              )
+              .formatted(
+                .measurement(
+                  width: .abbreviated,
+                  usage: .workout,
+                  numberFormatStyle: .number.precision(
+                    .fractionLength(0)
+                  )
+                )
+              )
+            )
+            
+            Text(
+              workoutManager.heartRate.formatted(
+                .number.precision(
+                  .fractionLength(0)
+                )
+              ) + " bpm"
+            )
+            
+            Text(
+              Measurement(
+                value: workoutManager.distance, 
+                unit: UnitLength.meters
+              )
+              .formatted(
+                .measurement(
+                  width: .abbreviated,
+                  usage: .road
+                )
+              )
+            )
+            
+            
+            Text(NSNumber(value: pace), formatter: timeFormatter)
+            
+            Text("avg pace")
+            Text("cadence")
+          }
+          .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+          .padding(.horizontal, 16)
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+      }
+      .font(.system(.title, design: .rounded).monospacedDigit().lowercaseSmallCaps())
+      .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+      .ignoresSafeArea()
+      .padding(0)
+      
+      
     }
+  }
 }
 
 struct MetricsView_Previews: PreviewProvider {
-    static var previews: some View {
-        MetricsView().environmentObject(WorkoutManager())
-    }
+  static var previews: some View {
+    MetricsView().environmentObject(WorkoutManager())
+  }
 }
 
 private struct MetricsTimelineSchedule: TimelineSchedule {
-    var startDate: Date
-    var isPaused: Bool
-
-    init(from startDate: Date, isPaused: Bool) {
-        self.startDate = startDate
-        self.isPaused = isPaused
+  var startDate: Date
+  var isPaused: Bool
+  
+  init(from startDate: Date, isPaused: Bool) {
+    self.startDate = startDate
+    self.isPaused = isPaused
+  }
+  
+  func entries(from startDate: Date, mode: TimelineScheduleMode) -> AnyIterator<Date> {
+    
+    var baseSchedule = PeriodicTimelineSchedule(
+      from: self.startDate,
+      by: (mode == .lowFrequency ? 1.0 : 1.0 / 30.0)
+    )
+      .entries(from: startDate, mode: mode)
+    
+    return AnyIterator<Date> {
+      guard !isPaused else { return nil }
+      return baseSchedule.next()
     }
-
-    func entries(from startDate: Date, mode: TimelineScheduleMode) -> AnyIterator<Date> {
-        var baseSchedule = PeriodicTimelineSchedule(from: self.startDate,
-                                                    by: (mode == .lowFrequency ? 1.0 : 1.0 / 30.0))
-            .entries(from: startDate, mode: mode)
-        
-        return AnyIterator<Date> {
-            guard !isPaused else { return nil }
-            return baseSchedule.next()
-        }
-    }
+  }
 }
